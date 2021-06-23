93d3557cad7802544db1a7f40bde7f34a3a2bb83
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-23 07:34:11 +0100

    added acronym model

---
+++ src/models/acronym.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+export interface AcronymAttributes {
+  code: string;
+  description: string;
+}
+
+export interface AcronymDocument extends Document {
+  code: string;
+  description: string;
+}
+
+interface AcronymModel extends Model<AcronymDocument> {
+  build(acronym: AcronymAttributes): AcronymDocument;
+}
+
+const AcronymSchema: Schema = new Schema({
+  code: { 
+    type: String,
+    required: true
+  },
+  description: { 
+    type: String, 
+    required: true
+  }
+}, { timestamps: true });
+
+AcronymSchema.statics.build = (acronym: AcronymAttributes) => {
+  return new Acronym(acronym);
+}
+
+const Acronym = mongoose.model<AcronymDocument, AcronymModel>('Acronym', AcronymSchema);
+
+export default Acronym;
