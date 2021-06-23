5453950a17f16c488797e15d4b272a6c504472f6
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-23 07:02:15 +0100

    jest setup

---
+++ test/setup.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+beforeAll(async () => {
+  const mongoUri = 'mongodb://localhost/g2i-node-test.test';
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
