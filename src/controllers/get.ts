80faf29f400b54fda52e1b55f6ee96fea915e20f
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-24 05:12:32 +0100

    added get endpoint

---
+++ src/controllers/get.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import DatabaseError from '../errors/database-error';
+import Acronym, { AcronymDocument } from '../models/acronym';
+
+const get = async (req: Request, res: Response): Promise<void> => {
+  const { code } = req.params as { code: string };
+  
+  let acronym: AcronymDocument | null; 
+  try {
+    acronym = await Acronym.findOne({ code });
+  } catch(err) {
+    throw new DatabaseError(`Error occured when retreiving acronym(${code})`, err.message);
+  }
+
+  res.status(200).send({ ...acronym });
+}
+
+export default get;
