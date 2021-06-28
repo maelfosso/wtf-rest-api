6db9bac26afaf39abcb31984e9973182cbeed78a
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-28 05:55:01 +0100

    improved the test of getAll

--- src/controllers/__test__/getAll.test.ts
+++ src/controllers/__test__/getAll.test.ts
@@ -7,24 +7,21 @@ const http = request(app);
 describe('GET /acronym', () => {
 
   test('return 200 for successful acronym found', async () => {
-    const code = faker.lorem.word();
-    const description = faker.lorem.word();
-
     const acronym = {
-      code,
-      description
+      code: faker.lorem.word(),
+      description: faker.lorem.sentence()
     }
     await http.post('/acronym')
       .send(acronym)
       .expect(201);
 
-    const response = await http.get(`/acronym/${code}`)
+    const response = await http.get(`/acronym`)
       .send();
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveLength(1)
-    expect(response.body[0].code).toEqual(code);
-    expect(response.body[0].description).toEqual(description);
+    expect(response.body[0].code).toEqual(acronym.code);
+    expect(response.body[0].description).toEqual(acronym.description);
   });
 
 });
