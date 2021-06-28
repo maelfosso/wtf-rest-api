a78cfd9ee47ba6561bd1db357da5e0a467073b96
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-28 06:02:30 +0100

    scaffold of delete test

---
+++ src/controllers/__test__/remove.test.ts
@@ -0,0 +1,16 @@
+
+describe('DELETE /acronym/:code', () => {
+
+  test('return 403 if no authorization token', async () => {
+
+  });
+
+  test('return 404 if code does not exist', async () => {
+
+  });
+
+  test('return 200 if successfully delete', async () => {
+
+  });
+
+})
